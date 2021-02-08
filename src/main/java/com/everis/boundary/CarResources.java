4df998aae61221513aee66ce2ab25feb1a8c0b2b
Abdelsbihi <abdelsbh2@gmail.com>
2021-02-08 09:36:23 +0100

    Add entering-exiting for every method

---
+++ src/main/java/com/everis/boundary/CarResources.java
@@ -0,0 +1,97 @@
+package com.everis.boundary;
+
+import java.util.List;
+
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.ws.rs.core.Response.*;
+
+import org.apache.log4j.Logger;
+import com.everis.boundary.CarResources;
+import com.everis.control.CarService;
+import com.everis.entity.Car;
+
+
+@Path("cars")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CarResources {
+
+    @EJB
+    CarService carService;
+    
+    private final static Logger LOGGER = Logger.getLogger(CarResources.class);
+
+    @GET
+    public Response getCars() {
+    		LOGGER.info("Retrieving Car's List from car service: ");
+    	try {
+    		List<Car> cars = carService.getCars();
+			LOGGER.info("Car's list retrieved");
+			return Response.ok().entity(cars).build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			LOGGER.info("Car's list not found");
+			return Response.status(Status.NOT_FOUND).build();
+		}   	
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getCarById(final @PathParam("id") int id)
+    {		
+    		LOGGER.info("Getting Car by its Id: " + id);
+	    try {
+			return Response.ok().entity(carService.getCar(id)).build();
+		} catch (Exception e) {
+			LOGGER.error("Car with id : " + id + " Not Found");
+			 return Response.status(Status.NOT_FOUND).build();
+		}
+    }
+
+    @POST
+    public Response createCar(final Car car) {
+    		LOGGER.info("Creating new Car: ");
+    	try {
+			LOGGER.info("new car created: "+car);
+			return Response.status(Status.CREATED).entity(carService.createCar(car)).build();
+		} catch (Exception e) {
+			LOGGER.error("Failed to create new car");
+		    return Response.status(Status.BAD_REQUEST).build();
+		}
+	}    
+
+    @PUT
+    @Path("/{id}")
+    public Response updateCar(final @PathParam("id") int id) {
+    	
+    	LOGGER.info("Update new car!");
+    	
+    	try {
+			Car newCar = this.carService.getCar(id);
+			this.carService.updateCar(newCar);
+			LOGGER.info("Car Successfully Updated: " + newCar + "Id: " + id);
+			return Response.status(Status.OK).entity(newCar).build();
+		} catch (Exception e) {
+			LOGGER.error("Error: Car not found!");
+			return Response.status(Status.NOT_FOUND).build();			
+		}	  
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteCar(final @PathParam("id") int id) {
+    		LOGGER.info("Deleting car with id:"+id);
+	    try {
+			this.carService.deleteCar(id);
+			LOGGER.info("Car with id " + id + " Deleted");
+			return Response.ok().entity("Car Deleted Successfully").build();
+		} catch (Exception e) {
+			LOGGER.error("Failed to delete Car with id " + id);
+		    return Response.status(Status.NOT_FOUND).entity("Car with id " + id + " not found").build();
+		}
+    }
+}
+    
---
+++ src/main/java/com/everis/control/CarService.java
@@ -0,0 +1,80 @@
+package com.everis.control;
+
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.log4j.Logger;
+
+import com.everis.control.CarService;
+import com.everis.entity.Car;
+
+@Stateless
+public class CarService {
+
+    @PersistenceContext(unitName = "postgres")
+    private EntityManager em;
+    
+    private final static Logger LOGGER = Logger.getLogger(CarService.class);
+
+    /**
+     * Method to get a list of Car Entity available
+     */
+    public List<Car> getCars()
+    {	
+    	LOGGER.info("Getting all Cars' List: ");
+		List<Car> listCars = em.createNamedQuery("Car.findAll", Car.class).getResultList();
+		LOGGER.info("Car's List completed: " + listCars);
+		return listCars;
+    }
+
+    /**
+     * Method to get one Car info using its id                             
+     */
+    public Car getCar(int id)
+    {
+    	LOGGER.info("Getting Car by it Id... ");
+    	Car car = em.createNamedQuery("Car.findById", Car.class).setParameter("id", id).getSingleResult();
+    	LOGGER.info("Car selected: " + car);
+    	return car;
+    	
+    }
+
+    /**
+     * Method to create Car 
+     */
+    public Car createCar(final Car car)
+    {
+    	LOGGER.info("Creating Car... ");
+    	em.persist(car);
+    	LOGGER.info("Created Car: " + car);
+    	return car;
+    }
+    /**
+     * Method to update Car using its id
+     */
+    public Car updateCar(Car car)
+    {
+    	LOGGER.info("Updating Car...");
+    	em.merge(car);
+    	LOGGER.info("Car updated " + car);
+    	return car;
+    }
+   
+    /**
+     * Method to delete Car using its id
+     *
+     */
+    public void deleteCar(int id)
+    {
+    	LOGGER.info("Deleting Car... ");
+    	final Car car = getCar(id);
+    	LOGGER.info("Car's Id chosen for delete: " + id);
+    	em.remove(car);
+    	LOGGER.info("Deleted Car: " + car);	
+    }
+
+}
