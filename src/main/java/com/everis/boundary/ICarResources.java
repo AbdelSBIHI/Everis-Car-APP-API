2bba183a59221691a849c2af93edf9d78339cbe6
Abdelsbihi <abdelsbh2@gmail.com>
2021-02-16 16:48:44 +0100

    add Javadocs

--- src/main/java/com/everis/boundary/ICarResources.java
+++ src/main/java/com/everis/boundary/ICarResources.java
@@ -1,57 +1,75 @@
 package com.everis.boundary;
 
-
-
 import javax.validation.Valid;
 import javax.ws.rs.core.Response;
 import com.everis.entity.Car;
-
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
-
+/**
+ * @author AbdelSBIHI interface for mapping the API with swagger documentation
+ */
 public interface ICarResources {
-	
 
-    
-    @Operation(description = "Get a list of cars")
-    @ApiResponse(responseCode = "200", description = "Returns List of Cars Available")
-    public Response getCars();
-    
+	/**
+	 * fetching all cars
+	 * 
+	 * @return Response
+	 */
+	@Operation(description = "Get a list of cars")
+	@ApiResponse(responseCode = "200", description = "Returns List of Cars Available")
+	public Response getCars();
 
-    
-    
-    @Operation(description = "Pick a Car by its Id", responses = {
-    	    @ApiResponse(responseCode = "200", description = "Returns a Car by its id"),
-    	    @ApiResponse(responseCode = "404", description = "Car with given id doesn't exists") })
-    @Parameter(description = "Refference of Car's Id selected by the user", required = true)
-    public Response getCarById(final String id);
-    
+	/**
+	 * Pick a Car by its Id
+	 * 
+	 * @param id id of the car
+	 * @return Response
+	 */
+	@Operation(description = "Pick a Car by its Id", responses = {
+			@ApiResponse(responseCode = "200", description = "Returns a Car by its id"),
+			@ApiResponse(responseCode = "404", description = "Car with given id doesn't exists") })
+	@Parameter(description = "Refference of Car's Id selected by the user", required = true)
+	public Response getCarById(final String id);
 
-    
-    @Operation(description = "Create new car", responses = {
-    	    @ApiResponse(responseCode = "201", description = "Car has been successfully created"),
-    	    @ApiResponse(responseCode = "400", description = "creation of a new car has been failed") })
-    @Parameter(description = "Refference of object car to be created", required = true)
-    public Response createCar(@Valid final Car car); 
-    
-    
-    
-    @Operation(description = "Update new car", responses = {
-    	    @ApiResponse(responseCode = "200", description = "Car has been successfully updated"),
-    	    @ApiResponse(responseCode = "400", description = "Car cannot be udpated"),
-    	    @ApiResponse(responseCode = "404", description = "Car with given id doesn't exists") } )
-        @Parameter(description = "Refference of Car's id to be updated", required = true)
-        @Parameter(description = "Refference of Object Car to be updated", required = true)
-    public Response updateCar(final String id ,@Valid final Car car);
-    
-    @Operation(description = "Delete existing car", responses = {
-    	    @ApiResponse(responseCode = "200", description = "Car has been successfully deleted"),
-    	    @ApiResponse(responseCode = "404", description = "Car with given id doesn't exists") } )
-    @Parameter(description = "Refference of Car's id selected by the user", required = true)
-    public Response deleteCar(final  String id); 
+	/**
+	 * create a new car
+	 * 
+	 * @param car the car to be created
+	 * @return Response
+	 */
+	@Operation(description = "Create new car", responses = {
+			@ApiResponse(responseCode = "201", description = "Car has been successfully created"),
+			@ApiResponse(responseCode = "400", description = "creation of a new car has been failed") })
+	@Parameter(description = "Refference of object car to be created", required = true)
+	public Response createCar(@Valid final Car car);
 
+	/**
+	 * update existing car
+	 * 
+	 * @param car car to be updated
+	 * @return Response
+	 */
+	@Operation(description = "Update new car", responses = {
+			@ApiResponse(responseCode = "200", description = "Car has been successfully updated"),
+			@ApiResponse(responseCode = "400", description = "Car cannot be udpated"),
+			@ApiResponse(responseCode = "404", description = "Car with given id doesn't exists") })
+	@Parameter(description = "Refference of Car's id to be updated", required = true)
+	@Parameter(description = "Refference of Object Car to be updated", required = true)
+	public Response updateCar(final String id, @Valid final Car car);
+
+	/**
+	 * delete an existing car
+	 * 
+	 * @param id id of the car to be deleted
+	 * @return Response
+	 */
+	@Operation(description = "Delete existing car", responses = {
+			@ApiResponse(responseCode = "200", description = "Car has been successfully deleted"),
+			@ApiResponse(responseCode = "404", description = "Car with given id doesn't exists") })
+	@Parameter(description = "Refference of Car's id selected by the user", required = true)
+	public Response deleteCar(final String id);
 
 }
--- src/main/java/com/everis/control/CarService.java
+++ src/main/java/com/everis/control/CarService.java
@@ -7,14 +7,20 @@ import javax.inject.Inject;
 import com.everis.control.CarService;
 import com.everis.entity.Car;
 
+/**
+ * @author AbdelSBIHI
+ *  
+ */
 @Stateless
 public class CarService {
-	
+
 	@Inject
 	private CarDao carDao;
 
 	/**
-	 * Method to get a list of Car Entity available
+	 * get a list of Car available
+	 * 
+	 * @return list of cars
 	 */
 
 	public List<Car> getCars() {
@@ -23,17 +29,22 @@ public class CarService {
 	}
 
 	/**
-	 * Method to get one Car info using its id
+	 * get one Car info using its id
 	 * 
+	 * @param id id of the car
+	 * @return a Car if the car exists, null if not
 	 */
-	public Car getCar(String id)  {
+	public Car getCar(String id) {
 		Car car = carDao.findOne(id);
 		return car;
 
 	}
 
 	/**
-	 * Method to create Car
+	 * create Car
+	 * 
+	 * @param car object contains information of the car the we want to create
+	 * @return Car: the saved car
 	 */
 	public Car createCar(final Car car) {
 		carDao.addCar(car);
@@ -41,7 +52,10 @@ public class CarService {
 	}
 
 	/**
-	 * Method to update Car using its id
+	 * update Car using its id
+	 * 
+	 * @param car contains the new info of an existing car
+	 * @return Car :a car with new informations
 	 */
 	public Car updateCar(String id, Car car) {
 		carDao.editCar(id, car);
@@ -49,8 +63,10 @@ public class CarService {
 	}
 
 	/**
-	 * Method to delete Car using its id
-	 *
+	 * delete Car using its id
+	 * 
+	 * @param id: the id of the card we want to delete
+	 * @return true if the car deleted, false if not
 	 */
 	public Boolean deleteCar(String id) {
 		try {
